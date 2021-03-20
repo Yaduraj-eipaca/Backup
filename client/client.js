3f5444cf44a8848eb4652a688d64dacee5d87ded
Master Yoda <63950560+Yaduraj-eipaca@users.noreply.github.com>
2021-03-20 23:32:03 +0100

    Add files via upload

---
+++ client/client.js
@@ -0,0 +1,34 @@
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+const API_URL = "http://localhost:5000/mews"
+
+
+
+loadingElement.style.display="none";
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+    
+
+
+    const mew = {
+        name,
+        content
+    };
+
+    
+    form.style.display='none'
+    loadingElement.style.display = '';
+
+    fetch(API_URL, {
+        method: 'post',
+        body: mew,
+        headers: {
+            'content-type' : 'application/json'
+        }
+    })
+});
